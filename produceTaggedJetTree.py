7c258ea12a77c68e14ee406ddd8d18dca6f74163
Sébastien <sebastien.wertz@uclouvain.be>
2015-05-30 16:53:18 +0200

    Track counting with MVA ok

--- produceTaggedJetTree.py
+++ produceTaggedJetTree.py
@@ -1,12 +1,11 @@
-from tools import *
 import os
-from trackCounting import gt, geqt, lt, leqt, eq, neq
+from trackCounting import gt, geq, lt, leq, eq, neq
 from trackCounting import produceTaggedJetTree
 
 storeDirectory = "/storage/data/cms/store/user/brfranco/bTag/QCD_Phys14/QCD_Pt-30to50_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8/crab_QCD_Pt-30to50_MuEnrichedPt5_PionKaonDecay_Tune4C_13TeV_pythia8/150306_172100/0000/"
 rootFileNames = ["JetTree_phys14_1.root"]
 treeDirectory = "btagana/ttree"
-outFile = "testTC.root"
+outFile = "../myTrees/jetTree_TC_btagCuts_MLP_Nplus5_noSel.root"
 
 trackCut = [ # bTag current selection
     ( "Track_nHitPixel", geq, 2 ),
@@ -20,8 +19,9 @@ trackCut = [ # bTag current selection
 ]
 #return HitPix >= 1 and HitAll>=6 and IP2D<0.3 and Pt>0.5 and Chi2 < 7 and ZIP < 20 and Length <60 and Dist < 0.1
 
-TrackVars = [
-
+# Variables used by TMVA
+# Caution! Needs to be same order as when the MVA was trained! (thank you TMVA)
+trackMVAVars = [
     "Track_zIP",
     "Track_length",
     "Track_dist",
@@ -29,10 +29,16 @@ TrackVars = [
     "Track_pt",
     "Track_chi2",
     "Track_nHitPixel",
-    "Track_nHitAll"
-
+    "Track_nHitAll",
 ]
+
+trackMVA = {
+        "path": "/home/fynu/bfrancois/MyCMSSWcode/bTag/bTag_track_optimisation/weights/MLP_Nplus5_noSel.weights.xml",
+        "name": "Nplus5_noSel",
+        "cut": 0.6,
+        "vars": trackMVAVars
+        }
 
 if __name__ == "__main__":
     fileList = [ os.path.join(storeDirectory, file) for file in rootFileNames ]
-    produceTaggedJetTree(fileList, treeDirectory, outFile)
+    produceTaggedJetTree(fileList, treeDirectory, outFile, trackCut=trackCut, trackMVA=trackMVA)
